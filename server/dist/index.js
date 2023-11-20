6a3a827771f3b26c2b85cd27246cd42889d1182c
Kishor Balgi <kishorbalgi@gmail.com>
2023-11-20 12:37:54 +0530

    build: es6

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,12 +1,7 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const app_1 = require("./app");
+import dotenv from "dotenv";
+dotenv.config();
+import { httpServer } from "./app";
 const port = process.env.PORT || 3000;
-app_1.httpServer.listen(port, () => {
+httpServer.listen(port, () => {
     console.log(`Server running on Port: ${port} http://localhost:${port}`);
 });
