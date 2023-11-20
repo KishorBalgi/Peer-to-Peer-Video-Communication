36c9ba63f955d66bf3e1813b41e0157f6ebcfd7f
Kishor Balgi <kishorbalgi@gmail.com>
2023-11-20 13:24:13 +0530

    deploy: untracked dist

--- server/dist/index.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import { httpServer } from "./app";
-const port = process.env.PORT || 3000;
-httpServer.listen(port, () => {
-    console.log(`Server running on Port: ${port} http://localhost:${port}`);
-});
