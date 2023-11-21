a2aecde79d8465d1f2ea4bb659ac5f7c58377107
Kishor Balgi <kishorbalgi@gmail.com>
2023-11-21 18:11:42 +0530

    feat: added jwt token to cookie

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, CookieOptions } from "express";
 import catchAsync from "../utils/catchAsync";
 import { sendResponse } from "../utils/response";
 import { signup, login } from "../services/auth.services";
+
+// Cookie Options for JWT:
+const addCookie = (res: Response, req: Request, token: string) => {
+  const cookieOps: CookieOptions = {
+    expires: new Date(Date.now() + 60 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+  };
+  if (process.env.NODE_ENV === "production") cookieOps.sameSite = "none";
+  res.cookie("jwt", token, cookieOps);
+};
 
 // Sign up:
 export const signupController = catchAsync(
@@ -14,6 +25,7 @@ export const signupController = catchAsync(
 
     const user = await signup({ name, email, password });
 
+    addCookie(res, req, user.jwttoken);
     sendResponse(res, 201, user);
   }
 );
@@ -29,6 +41,7 @@ export const loginController = catchAsync(
 
     const user = await login({ email, password });
 
+    addCookie(res, req, user.jwttoken);
     sendResponse(res, 200, user);
   }
 );
