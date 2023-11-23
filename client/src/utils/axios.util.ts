951d583b0a6b191c8f3a35a3a58030b32d65c7b6
Kishor Balgi <kishorbalgi@gmail.com>
2023-11-23 15:46:22 +0530

    feat: axios config

--- client/src/utils/axios.util.ts
+++ client/src/utils/axios.util.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,
   withCredentials: true,
 });
 
